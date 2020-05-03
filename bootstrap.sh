541270989a3aca74fda2b9aa654d75b99911e1f1
djwelch <djwelch666@googlemail.com>
2020-05-03 21:00:59 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,16 @@
 
 mkdir -p ~/.local/{applications,bin}
 
+# zsh {{{
+sudo apt install zsh 
+chsh -s $(which zsh)
+# }}}
+
+# fzf {{{
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+# }}}
+
 # nvim {{{
 curl -L --create-dirs -o ~/.local/applications/nvim.appimage \
   https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
