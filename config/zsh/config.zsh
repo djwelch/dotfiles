840127b1d3c3c593a0c44cf67b2686d5fa139fd3
David Welch <david.welch@noreply>
2020-06-11 22:28:29 +0100

    .

--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -9,7 +9,10 @@ export EDITOR=nvim
 
 [ -f ~/.config/zsh/history.zsh ] && source ~/.config/zsh/history.zsh
 
-export PYENV_ROOT="$HOME/.pyenv"
+source /usr/share/fzf/key-bindings.zsh
+source /usr/share/fzf/completion.zsh
+
+# export PYENV_ROOT="$HOME/.pyenv"
 fpath+=$HOME/.config/zsh/pure
 path+=("$HOME/.local/bin")
 path+=("$HOME/.rbenv/bin")
@@ -17,19 +20,19 @@ path+=("$PYENV_ROOT/bin")
 export PATH
 export PKG_CONFIG_PATH=~/.local/lib/pkgconfig
  
-alias newspaste="win32yank.exe -o >> ~/.config/newsboat/urls && vim + ~/.config/newsboat/urls"
+# alias newspaste="win32yank.exe -o >> ~/.config/newsboat/urls && vim + ~/.config/newsboat/urls"
 
-export BROWSER='/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe'
+# export BROWSER='/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe'
 # export PURE_PROMPT_SYMBOL=🐻
 export PURE_PROMPT_SYMBOL=🐼
 autoload -U promptinit; promptinit
 prompt pure
 
-[ -f ~/.config/zsh/motd.zsh ] && source ~/.config/zsh/motd.zsh
+# [ -f ~/.config/zsh/motd.zsh ] && source ~/.config/zsh/motd.zsh
 
-eval "$(rbenv init -)"
-eval "$(pyenv init -)"
-source $HOME/.cargo/env
+# eval "$(rbenv init -)"
+# eval "$(pyenv init -)"
+# source $HOME/.cargo/env
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
---
+++ old.bootstrap.sh
@@ -0,0 +1,173 @@
+# /bin/sh 
+sudo apt-get update
+sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev \
+   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev \
+   libxmlsec1-dev libffi-dev liblzma-dev python-openssl unzip
+
+mkdir -p ~/.local/{applications,bin}
+[ ! -f ~/.ssh/github_id_rsa ] && ssh-keygen -f ~/.ssh/github_id_rsa
+[ ! -f ~/.ssh/gitlab_id_rsa ] && ssh-keygen -f ~/.ssh/gitlab_id_rsa
+
+# figlet {{{
+if [ ! -f ~/.local/bin/figlet ]; then
+  curl -L --create-dirs -o ~/tmp/figlet.tar.gz \
+    ftp://ftp.figlet.org/pub/figlet/program/unix/figlet-2.2.5.tar.gz
+  tar -xvf ~/tmp/figlet.tar.gz -C ~/tmp/
+  oldpath=`pwd`
+  cd ~/tmp/figlet-2.2.5
+  make
+  cp {figlet,figlist,showfigfonts} ~/.local/bin/.
+  cd oldpath
+  curl -L --create-dirs -o ~/.local/share/figlet/puffy.flf \
+    http://www.figlet.org/fonts/puffy.flf
+  curl -L --create-dirs -o ~/.local/share/figlet/Small.flf \
+    https://raw.githubusercontent.com/xero/figlet-fonts/master/Small.flf
+  curl -L --create-dirs -o ~/.local/share/figlet/twopoint.flf \
+    https://raw.githubusercontent.com/xero/figlet-fonts/master/twopoint.flf
+  curl -L --create-dirs -o ~/.local/share/figlet/Standard.flf \
+    https://raw.githubusercontent.com/xero/figlet-fonts/master/Standard.flf
+fi
+rm -f ~/tmp/figlet.tar.gz
+rm -rf ~/tmp/figlet-2.2.5
+# }}}
+
+# lolcat {{{
+if [ ! -f ~/.local/bin/lolcat ]; then
+  git clone https://github.com/dosentmatter/lolcat.git ~/tmp/lolcat
+  oldpath=`pwd`
+  cd ~/tmp/lolcat
+  make lolcat
+  cp lolcat ~/.local/bin/.
+  cd oldpath
+fi
+rm -rf ~/tmp/lolcat
+# }}}
+
+# fonts {{{
+if [ ! -d /mnt/c/Users/djwel/Fonts ]; then
+  curl -L --create-dirs -o ~/tmp/FiraCode.zip \
+    https://github.com/tonsky/FiraCode/releases/download/3.1/FiraCode_3.1.zip
+  unzip ~/tmp/FiraCode.zip -d ~/tmp/FiraCode
+  mkdir /mnt/c/Users/djwel/Fonts
+  find ~/tmp/FiraCode -iname "*.ttf" | grep /ttf/ | xargs -I{} cp {} /mnt/c/Users/djwel/Fonts/.
+  explorer.exe `wslpath -w /mnt/c/Users/djwel/Fonts`
+fi
+rm -f ~/tmp/FiraCode.zip
+rm -rf ~/tmp/FiraCode
+# }}}
+
+# win32yank {{{
+# requires https://www.microsoft.com/en-gb/download/confirmation.aspx?id=48145
+if [ ! -f ~/.local/bin/win32yank.exe ]; then
+  curl -L --create-dirs -o ~/tmp/win32yank-x64.zip \
+    https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
+  unzip ~/tmp/win32yank-x64.zip -d ~/tmp/win32yank-x64
+  cp ~/tmp/win32yank-x64/win32yank.exe ~/.local/bin/.
+  chmod a+x ~/.local/bin/win32yank.exe
+fi
+rm -f ~/tmp/win32yank-x64.zip
+rm -rf ~/tmp/win32yank-x64
+# }}}
+
+# zsh {{{
+sudo apt install zsh 
+chsh -s $(which zsh)
+[ ! -f ~/.zshrc ] && echo '[ -f ~/.config/zsh/config.zsh ] && source ~/.config/zsh/config.zsh' > ~/.zshrc
+
+if [ ! -d ~/.config/zsh/pure ]; then
+  git clone https://github.com/sindresorhus/pure.git ~/.config/zsh/pure
+fi
+# }}}
+
+# fzf {{{
+if [ ! -d ~/.fzf ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --update-rc --key-bindings --completion
+fi
+# }}}
+
+# nvim {{{
+if [ ! -f ~/.local/bin/nvim ]; then
+  curl -L --create-dirs -o ~/.local/applications/nvim.appimage \
+    https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+  chmod a+x ~/.local/applications/nvim.appimage
+  ln -s -r ~/.local/applications/nvim.appimage ~/.local/bin/nvim 
+fi
+# }}}
+
+
+# stfl {{{
+if [ ! -f ~/.local/lib/libstfl.so ]; then
+  curl -L --create-dirs -o ~/tmp/stfl.tar.gz \
+    http://www.clifford.at/stfl/stfl-0.24.tar.gz
+  tar -xvf ~/tmp/stfl.tar.gz -C ~/tmp/
+  oldpath=`pwd`
+  cd ~/tmp/stfl-0.24
+  DESTDIR="~/" prefix=".local" make
+  DESTDIR="~/" prefix=".local" make install
+  cd oldpath
+  echo Does not install the correct pc config file
+fi
+# }}}
+rm -f ~/tmp/stfl.tar.gz
+rm -rf ~/tmp/stfl-0.24
+
+# ruby {{{
+if [ ! -d ~/.rbenv ]; then
+  curl -sL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash 
+  # rbenv install 2.5.1
+  # rbenv global 2.5.1
+fi
+# }}}
+
+# rust {{{
+if [ ! -d ~/.cargo/bin ]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+# }}}
+
+# # newsboat {{{
+# if [ ! -f ~/.local/bin/newsboat ]; then
+#   gem install asciidoctor
+#   curl -L --create-dirs -o ~/tmp/newsboat.tar.xz \
+#     https://github.com/newsboat/newsboat/archive/r2.19.tar.gz
+#   tar -xvf ~/tmp/newsboat.tar.xz -C ~/tmp/
+#   oldpath=`pwd`
+#   cd ~/tmp/newsboat-r2.19
+#   prefix="~/.local" make
+#   prefix="~/.local" make install
+#   cd oldpath
+# fi
+# # }}}
+# rm -f ~/tmp/newsboat.tar.xz 
+# rm -rf ~/tmp/newsboat-2.18
+
+# youtube-dl {{{
+if [ ! -f ~/.local/bin/youtube-dl ]; then
+  curl -L https://yt-dl.org/downloads/latest/youtube-dl -o ~/.local/bin/youtube-dl
+  chmod a+rx ~/.local/bin/youtube-dl
+fi
+# }}}
+
+# pyenv {{{
+if [ ! -d ~/.pyenv ]; then
+  git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+fi
+# }}}
+
+# ffmpeg {{{
+if [ ! -f ~/.local/bin/ffmpeg ]; then
+  curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ~/tmp/ffmpeg.tar.xz
+  tar -xvf ~/tmp/ffmpeg.tar.xz -C ~/tmp/
+  cp ~/tmp/ffmpeg-4.2.2-amd64-static/ffmpeg ~/.local/bin/ffmpeg
+  cp ~/tmp/ffmpeg-4.2.2-amd64-static/ffprobe ~/.local/bin/ffprobe
+fi
+# }}}
+
+# nvm {{{
+if [ ! -d ~/.nvm ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+  sudo apt install --no-install-recommends yarn
+fi
+# }}}
+
