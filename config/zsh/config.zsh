e2e86bc3f8e6009f64905c62fde39062e22ca1be
David <david@anon>
2020-11-12 08:50:48 +0000

    Add rust

--- config/zsh/config.zsh
+++ config/zsh/config.zsh
@@ -19,6 +19,7 @@ path+=("$HOME/.local/dotnet")
 path+=("$HOME/.rbenv/bin")
 path+=("$HOME/.yarn/bin")
 path+=("$HOME/.config/yarn/global/node_modules/.bin")
+path+=("$HOME/.cargo/bin")
 export PATH
 export PKG_CONFIG_PATH=~/.local/lib/pkgconfig
 export DOTNET_ROOT=/home/david/.local/dotnet
